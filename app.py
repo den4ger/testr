8ac31973961aaf4c8b1355099ae8bae83ebe2cf2
Deniz Abdullaev <114094217+den4ger@users.noreply.github.com>
2026-06-20 18:10:53 +0300

    v2

--- app.py
+++ app.py
@@ -99,11 +99,11 @@ if page == "Прогноз по регионам":
              region_eval[['date', 'pred_total_demand']]]).sort_values('date')
 
 
-        # 95 интервал
+        # 95% интервал
 
         Q_ERROR_CONFORMAL = 27322.37
 
-        # Строим коридоры доверительного интервала вокруг прогноза
+        # коридоры доверительного интервала вокруг прогноза
         pred_line['lower_bound'] = pred_line['pred_total_demand'] - Q_ERROR_CONFORMAL
         pred_line['upper_bound'] = pred_line['pred_total_demand'] + Q_ERROR_CONFORMAL
 
@@ -156,11 +156,11 @@ elif page == "EDA по датасету":
     col1, col2, col3 = st.columns(3)
     col1.metric("Всего исторических записей", len(df_full_train))
     col2.metric("Количество уникальных регионов", df_full_train['region_code'].nunique())
-    col3.metric("Средний чек спроса по РФ", f"{round(df_full_train[TARGET_COL].mean(), 2)} млн руб.")
+    col3.metric("Средний спрос по регионам РФ", f"{round(df_full_train[TARGET_COL].mean(), 2)} млн руб.")
 
     st.markdown("---")
 
-    #  График распределения таргета (с логарифмом и без)
+    #  График распределения таргета
     st.subheader("Распределение целевой переменной")
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 5))
 
@@ -171,12 +171,148 @@ elif page == "EDA по датасету":
     ax2.set_title("Логарифмированное распределение")
     st.pyplot(fig)
     plt.close(fig)
+    st.markdown("---")
+    st.subheader("Матрица корреляций признаков")
 
+
+    cols_to_exclude = ['year', 'quarter', 'month', 'region_code', 'timezone', 'election_year']
+    corr_cols = [col for col in [
+        'total_demand_mil_rub', 'gdp_growth', 'gdp_per_capita', 'inflation_cpi',
+        'usd_rub_rate', 'brent_price', 'urals_price', 'rts_index', 'moex_index',
+        'budget_deficit', 'international_reserves', 'money_supply_m2', 'credit_growth',
+        'avg_deposit_rate', 'avg_mortgage_rate', 'mortgage_issued_volume',
+        'mortgage_issued_count', 'total_mortgage_debt', 'population', 'population_growth',
+        'population_density', 'birth_rate', 'mortality_rate', 'natural_growth',
+        'net_migration', 'share_pre_working_age', 'share_working_age', 'share_post_working_age',
+        'marriage_rate', 'divorce_rate', 'avg_salary', 'median_salary', 'real_income_growth',
+        'unemployment_rate', 'employment_rate', 'grp', 'grp_per_capita', 'tourist_region',
+        'industrial_region', 'max_state_subsidy_amount', 'sqm_per_subsidy', 'price_to_salary_ratio',
+        'mortgage_affordability_index'
+    ] if col in df_full_train.columns]
+
+    # Вычисляем матрицу корреляции Пирсона
+    corr_matrix = df_full_train[corr_cols].corr(method = 'pearson')
+
+    # Комбобокс для выбора уровня фильтрации
+    filter_level = st.selectbox(
+        "Уровень фильтрации корреляций:",
+        options=[
+            "Показать все корреляции",
+            "Только заметные (> 0.3)",
+            "Только сильные (> 0.6)",
+            "Только очень сильные (> 0.9)"
+        ],
+        index=1
+    )
+
+    # Определяем порог
+    if "0.3" in filter_level:
+        threshold = 0.3
+    elif "0.6" in filter_level:
+        threshold = 0.6
+    elif "0.9" in filter_level:
+        threshold = 0.9
+    else:
+        threshold = None
+
+    if threshold is not None:
+        import numpy as np
+
+        # Переводим матрицу в обычный NumPy массив и делаем копию
+        corr_array = corr_matrix.to_numpy().copy()
+
+        # Зануляем главную диагональ
+        np.fill_diagonal(corr_array, 0)
+
+        # поиск индексов признаков, у которых осталась хотя бы одна связь выше порога
+        keep_mask = (np.abs(corr_array) >= threshold).any(axis=1)
+        keep_features = corr_matrix.index[keep_mask]
+
+        #  Срезаем исходную матрицу
+        corr_matrix = corr_matrix.loc[keep_features, keep_features]
+
+        st.caption(
+            f"Отображаются признаки со связями выше **{threshold}**. Доступно: **{len(keep_features)}** из {len(corr_cols)}.")
+
+    # 2. Строим интерактивную тепловую карту через Plotly
+    import plotly.express as px
+
+    if not corr_matrix.empty:
+        show_text = ".2f" if len(corr_matrix) <= 25 else False
+
+        fig_corr = px.imshow(
+            corr_matrix,
+            text_auto=show_text,
+            aspect="auto",
+            color_continuous_scale="RdBu_r",
+            zmin=-1, zmax=1,
+            title=f"Интерактивная тепловая карта"
+        )
+
+        fig_corr.update_traces(xgap=1, ygap=1)
+        fig_corr.update_layout(
+            width=1000,
+            height=900,
+            xaxis_tickangle=-45,
+            plot_bgcolor='rgba(0,0,0,0)'
+        )
+        st.plotly_chart(fig_corr, use_container_width=True)
+
+    st.markdown("---")
+
+    st.subheader("Анализ специфики регионов (Туризм и Промышленность)")
+
+    # Переменная цены за кв.м. (используем лаг 1, так как он есть в твоем списке признаков)
+    # Если в df_full_train есть базовая колонка 'price_per_sqm', замени на неё.
+    price_col = 'price_per_sqm_lag_1' if 'price_per_sqm_lag_1' in df_full_train.columns else 'price_per_sqm'
+
+    # Создаем две вкладки: одна для Цен, другая для Спроса
+    tab_prices, tab_demand = st.tabs(["Анализ цен за м²", "Анализ совокупного спроса"])
+
+    # Вкладка 1: Цены
+    with tab_prices:
+        fig_prices, axes_p = plt.subplots(1, 2, figsize=(14, 5))
+
+        sns.barplot(data=df_full_train, x="tourist_region", y=price_col, ax=axes_p[0], palette='Blues',
+                    hue="tourist_region", legend=False)
+        axes_p[0].set_title("Цена за м²: туристические регионы")
+        axes_p[0].set_xlabel("Туристический регион")
+        axes_p[0].set_ylabel("Цена за м²")
+
+        sns.barplot(data=df_full_train, x="industrial_region", y=price_col, ax=axes_p[1], palette='Greens',
+                    hue="industrial_region", legend=False)
+        axes_p[1].set_title("Цена за м²: промышленные регионы")
+        axes_p[1].set_xlabel("Промышленный регион")
+        axes_p[1].set_ylabel("Цена за м²")
+
+        st.pyplot(fig_prices)
+        plt.close(fig_prices)
+
+    # Вкладка 2: Совокупный спрос
+    with tab_demand:
+        fig_demand, axes_d = plt.subplots(1, 2, figsize=(14, 5))
+
+        sns.barplot(data=df_full_train, x="tourist_region", y="total_demand_mil_rub", ax=axes_d[0], palette='Blues',
+                    hue="tourist_region", legend=False)
+        axes_d[0].set_title("Совокупный спрос: туристические регионы")
+        axes_d[0].set_xlabel("Туристический регион (0 = Нет, 1 = Да)")
+        axes_d[0].set_ylabel("Спрос (млн руб.)")
+
+        sns.barplot(data=df_full_train, x="industrial_region", y="total_demand_mil_rub", ax=axes_d[1], palette='Greens',
+                    hue="industrial_region", legend=False)
+        axes_d[1].set_title("Совокупный спрос: промышленные регионы")
+        axes_d[1].set_xlabel("Промышленный регион (0 = Нет, 1 = Да)")
+        axes_d[1].set_ylabel("Спрос (млн руб.)")
+
+        st.pyplot(fig_demand)
+        plt.close(fig_demand)
+    st.info("Туристические и промышленные регионы характеризуются более высоким совокупным спросом и более высокими ценами на недвижимость.\n")
+    st.markdown("---")
     # Просмотр пропусков и типов данных
     if st.checkbox("Показать структуру и типы колонок"):
         st.dataframe(df_full_train.dtypes.astype(str).to_frame(name='Тип данных'))
 
-#метрики качесвта моделей
+#Метрики качества моделей
 
 elif page == "Метрики качества моделей":
     st.title("Сравнение конфигураций моделей")
